b6162bedadfdcc0d259816cfcd3947e7adea5679
s464921 <jedrzejbiesek@wp.pl>
2021-10-04 16:41:18 +0200

    commit 1

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,13 @@
 package introduction;
 
+import java.util.logging.Logger;
+
 public class HelloWorld {
+
+    final static Logger logger = Logger.getLogger("HelloWorld.class");
 
     public static void main(String [ ] args) {
         System.out.println("Hello! Witaj!");
+        logger.info("hahaha");
     }
 }
